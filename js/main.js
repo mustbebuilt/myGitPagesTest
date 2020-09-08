7f7a39b2657de2215e21a28861d6447e5b8b3249
Martin Cooper <martincooper@Martins-iMac-2.local>
2020-09-08 15:56:24 +0100

    Fuller Version

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// IIFE
+(function () {
+  // start code
+  // variables
+  var burgerMenu = document.getElementById("burgerMenu");
+  var burgerMenuContainer = document.querySelector(".burgerMenuContainer");
+  var navBar = document.querySelector("nav");
+  var navHeight = navBar.offsetHeight;
+  var navBarStatus = false;
+  // mobile menu
+  if (window.getComputedStyle(burgerMenuContainer).display !== "none") {
+    navBar.style.height = "0px";
+    burgerMenu.addEventListener("click", function () {
+      if (navBarStatus) {
+        navBarStatus = false;
+        navBar.style.height = "0px";
+        burgerMenu.setAttribute("src", "images/whiteMenu.svg");
+      } else {
+        navBarStatus = true;
+        navBar.style.height = navHeight + "px";
+        burgerMenu.setAttribute("src", "images/whiteClose.svg");
+      }
+    });
+  }
+  // end code
+})();
